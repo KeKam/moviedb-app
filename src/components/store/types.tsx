5f45645cabc02372b30f1c0eca26250f0972db5c
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2020-03-25 14:01:25 +0100

    Remove interfaces for actions

--- src/components/store/types.tsx
+++ src/components/store/types.tsx
@@ -1,17 +1,3 @@
-interface StartSearchMoviesAction {
-  type: ActionTypes.START_SEARCH_MOVIES;
-}
-
-interface SearchMoviesSuccessAction {
-  type: ActionTypes.SEARCH_MOVIES_SUCCESS;
-  payload: [];
-}
-
-interface SearchMoviesFailureAction {
-  type: ActionTypes.SEARCH_MOVIES_FAILURE;
-  payload: string;
-}
-
 export enum ActionTypes {
   START_SEARCH_MOVIES = 'START_SEARCH_MOVIES',
   SEARCH_MOVIES_SUCCESS = 'SEARCH_MOVIES_SUCCESS',
@@ -19,6 +5,14 @@ export enum ActionTypes {
 }
 
 export type Actions =
-  | StartSearchMoviesAction
-  | SearchMoviesSuccessAction
-  | SearchMoviesFailureAction;
+  | {
+      type: ActionTypes.START_SEARCH_MOVIES;
+    }
+  | {
+      type: ActionTypes.SEARCH_MOVIES_SUCCESS;
+      payload: [];
+    }
+  | {
+      type: ActionTypes.SEARCH_MOVIES_FAILURE;
+      payload: string;
+    };
