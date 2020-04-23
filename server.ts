ff4daaefb5ee6e817deb84219e7aa9717db07433
Kenta Kamimura <kenta.stolt.kamimura@gmail.com>
2020-04-23 18:51:21 +0200

    Add typescript to server

--- server.ts
+++ server.ts
@@ -1,11 +1,11 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const enforce = require('express-sslify');
-const compression = require('compression');
-const path = require('path');
+import express, { Application, Request, Response } from 'express';
+import cors from 'cors';
+import bodyParser from 'body-parser';
+import enforce from 'express-sslify';
+import compression from 'compression';
+import path from 'path';
 
-const app = express();
+const app: Application = express();
 const port = process.env.PORT || 5000;
 
 app.use(bodyParser.json());
@@ -18,12 +18,11 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, 'client/build')));
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
 
-  app.get('*', function (req, res) {
+  app.get('*', function (req: Request, res: Response) {
     res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
   });
 }
 
-app.listen(port, (error) => {
-  if (error) throw error;
+app.listen(port, () => {
   console.log('Server is running on port ' + port);
 });
